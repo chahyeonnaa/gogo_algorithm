6ae3136d21731dd3dff9fca73f62d38c2c3e9f69
Minseonhong <80440122+Minseonhong@users.noreply.github.com>
2022-04-09 15:15:18 +0900

    부족한 금액 계산하기/(#41)

---
+++ Seonhong/부족한 금액 계산하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count) {
+    long long answer = 0;
+
+    for (int i = 1; i <= count; i++)
+    {
+        answer = answer + price * i;
+    }
+    if (answer > money)
+        return answer - money;
+    else
+        return 0;
+}
