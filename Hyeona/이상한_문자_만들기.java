6d9bbb4fe61853e380bd9847b6cab92ec0026b6e
chahyeonnaa <gusdk21650@naver.com>
2022-05-31 17:41:37 +0900

    이상한 문자 만들기/(#54)

---
+++ Hyeona/이상한_문자_만들기.java
@@ -0,0 +1,27 @@
+package Hyeona;
+
+public class 이상한_문자_만들기 {
+    public String solution(String s) {
+        String answer = "";
+        int idx = 0;
+
+        String[] strArr = s.split("");
+
+        for(int i=0; i<strArr.length; i++) {
+            if((" ").equals(strArr[i])) idx = 0;
+            else {
+                if(idx%2==0) {
+                    idx++;
+                    strArr[i] = strArr[i].toUpperCase();
+                }else {
+                    idx++;
+                    strArr[i] = strArr[i].toLowerCase();
+                }
+            }
+            answer += strArr[i];
+        }
+
+        return answer;
+
+    }
+}
